8198126d8e97b960a923ee7e6cf9ccaf1b55776c
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-11-08 21:48:12 +0700

    chore: initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from './components/global-style/GlobalStyle';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    path: '/',
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle>
+      <RouterProvider router={router} />
+    </GlobalStyle>
   </React.StrictMode>
 );
 
