7b94b9b0aefad2a3c7a141a46ce49352fb6d6c37
vutiendat3601 <vutien.dat.3601@gmail.com>
2024-02-28 15:33:23 +0700

    Updates

--- OidcTrustedDomains.js
+++ OidcTrustedDomains.js
@@ -5,11 +5,27 @@
 // Domains used by OIDC server must be also declared here
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const trustedDomains = {
-    default: ['https://bbauth.datvu.tech', 'http://localhost:3000'],
-    config_classic: ['https://bbauth.datvu.tech'],
-    config_without_silent_login: ['http://localhost:3000'],
-    // config_without_refresh_token: ['https://demo.duendesoftware.com'],
-    // config_without_refresh_token_silent_login: ['https://demo.duendesoftware.com'],
-    // config_google: ['https://oauth2.googleapis.com', 'https://openidconnect.googleapis.com'],
-    // config_with_hash: ['https://demo.duendesoftware.com'],
+    default: ['https://bbapi.datvu.tech', 'https://bbauth.datvu.tech'],
+    config_classic: ['https://bbapi.datvu.tech'],
+    config_without_silent_login: ['https://bbapi.datvu.tech'],
+    config_without_refresh_token: ['https://bbapi.datvu.tech'],
+    config_without_refresh_token_silent_login: ['https://bbapi.datvu.tech'],
+    config_google: ['https://oauth2.googleapis.com', 'https://openidconnect.googleapis.com'],
+    config_with_hash: ['https://bbapi.datvu.tech'],
+};
+
+// Service worker will continue to give access token to the JavaScript client
+// Ideal to hide refresh token from client JavaScript, but to retrieve access_token for some
+// scenarios which require it. For example, to send it via websocket connection.
+trustedDomains.config_show_access_token = { 
+    domains: ['https://bbapi.datvu.tech'], 
+    showAccessToken: true,
+    // convertAllRequestsToCorsExceptNavigate: false,
+    // setAccessTokenToNavigateRequests: true,
+};
+
+// This example defines domains used by OIDC server separately from domains to which access tokens will be injected.
+trustedDomains.config_separate_oidc_access_token_domains = {
+    oidcDomains: ['https://bbapi.datvu.tech'],
+    accessTokenDomains: ['https://bbapi.datvu.tech'],
 };
